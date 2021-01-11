9180aece7a87e8c5167a6439f45431dcbbbcb6ec
Nikolai R. T. Povlsen <nikolaipovlsen@yahoo.dk>
2021-01-11 10:45:08 +0100

    Asks for GPS permission

--- WeatherApp/WeatherApp/WeatherApp/MainPage.xaml.cs
+++ WeatherApp/WeatherApp/WeatherApp/MainPage.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace WeatherApp
@@ -13,15 +15,47 @@ namespace WeatherApp
         public MainPage()
         {
             InitializeComponent();
+            getLocation();
+        }
 
-            if (Device.RuntimePlatform == Device.iOS)
-            {
-                //iOS stuff
-            }
-            else if (Device.RuntimePlatform == Device.Android)
-            {
+        CancellationTokenSource cts;
 
+        async Task getLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+                cts = new CancellationTokenSource();
+                var location = await Geolocation.GetLocationAsync(request, cts.Token);
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}");
+                }
             }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+                // Handle not enabled on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+        }
+
+        protected override void OnDisappearing()
+        {
+            if (cts != null && !cts.IsCancellationRequested)
+                cts.Cancel();
+            base.OnDisappearing();
         }
     }
 }
