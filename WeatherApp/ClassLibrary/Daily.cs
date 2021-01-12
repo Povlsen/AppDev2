14cfa945e2714b961ec7cd62c84869682dfe9585
Nikolai R. T. Povlsen <nikolaipovlsen@yahoo.dk>
2021-01-12 11:34:38 +0100

    Added Classes

---
+++ WeatherApp/ClassLibrary/Daily.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Daily
+    {
+        public int dt { get; set; }
+        public int sunrise { get; set; }
+        public int sunset { get; set; }
+        public Temp temp { get; set; }
+        public FeelsLike feels_like { get; set; }
+        public int pressure { get; set; }
+        public int humidity { get; set; }
+        public double dew_point { get; set; }
+        public double wind_speed { get; set; }
+        public int wind_deg { get; set; }
+        public List<Weather> weather { get; set; }
+        public int clouds { get; set; }
+        public int pop { get; set; }
+        public double uvi { get; set; }
+    }
+}
---
+++ WeatherApp/ClassLibrary/Minutely.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Minutely
+    {
+        public int dt { get; set; }
+        public int precipitation { get; set; }
+    }
+}
