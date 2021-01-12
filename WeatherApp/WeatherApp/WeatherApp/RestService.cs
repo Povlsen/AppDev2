09b7b01db9eb2db73b4ec9024cb115064772788c
Nikolai R. T. Povlsen <nikolaipovlsen@yahoo.dk>
2021-01-12 08:52:53 +0100

    Added RestService.cs

---
+++ WeatherApp/WeatherApp/WeatherApp/RestService.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherApp
+{
+    internal static class RestService
+    {
+        public static async Task<T> GetData<T>(string url) where T : class
+        {
+            T res = null;
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    res = JsonConvert.DeserializeObject<T>(content);
+                }
+                return res;
+            }
+        }
+    }
+}
